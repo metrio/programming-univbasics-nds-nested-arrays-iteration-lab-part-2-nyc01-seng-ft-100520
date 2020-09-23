53d700d03d5ad542d25860cbf103d82e1c8a92a8
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-23 20:37:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,12 @@
 def find_min_in_nested_arrays(src)
 row_index = 0 
-lowest_int = []
+lowest_int = [] #array to put in all of our lowest values
   while row_index < src.count do
     element_index = 0 
-    min = src[row_index][0]
+    min = src[row_index][0] #first number in array is lowest
         while element_index < src[row_index].count do
-          if src[row_index][element_index] < min
-             min = src[row_index][element_index]
+          if src[row_index][element_index] < min 
+             min = src[row_index][element_index] 
           end
         element_index += 1 
         end
